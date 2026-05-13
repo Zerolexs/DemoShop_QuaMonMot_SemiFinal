2d595a75d98b41102fc16e925492b995a28cbf90
Zerolexs <tienbaoluu@gmail.com>
2026-05-13 08:11:30 +0700

    Thêm Privacy

--- DemoShop_QuaMonMot/Controllers/HomeController.cs
+++ DemoShop_QuaMonMot/Controllers/HomeController.cs
@@ -140,5 +140,9 @@ namespace DemoShop_QuaMonMot.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
